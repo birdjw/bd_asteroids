7c2b792985775c50bdf75291c0032a1ec7829f5c
hotwhiskey <joshuawaynebird@gmail.com>
2025-05-22 16:32:49 -0400

    fps set

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 from constants import *
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
