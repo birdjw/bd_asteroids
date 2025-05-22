eac69a8af2a0b2fd550124e0fcfa1c779aa47afb
hotwhiskey <joshuawaynebird@gmail.com>
2025-05-22 11:35:35 -0400

    game window working

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ def main():
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
-    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
+
+    
