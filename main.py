58cf4b3869e5cabd4cfb7042d3d7abf53c882c4c
hotwhiskey <joshuawaynebird@gmail.com>
2025-05-28 16:00:44 -0400

    player rotation is working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
@@ -9,12 +9,15 @@ def main():
     print("Screen width: 1280")
     print("Screen height: 720")
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
